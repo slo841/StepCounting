cea7790d92753697cb731ba047f343d3886bd193
slo841 <frodol2012@gmail.com>
2016-10-18 11:37:58 -0700

    Tester class added

--- src/Tester.java
+++ src/Tester.java
@@ -1,4 +1,9 @@
+import java.util.Arrays;
 
 public class Tester {
-
-}
+	public static void main(String[] args) {
+		String[] columnNames = {"time", "gyro-x", "gyro-y", "gyro-z"};
+		CSVData data = new CSVData("C:\\Users\\slo841\\workspace\\CSVData\\Test", columnNames, 1);
+		System.out.println((data));
+	}
+ }
